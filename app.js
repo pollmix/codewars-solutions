f9b9f97a0fdbbb4b8129474bef896b44febbc33d
pollmix <polboy777@gmail.com>
2021-02-25 12:22:03 +0600

    update

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const inquirer = require("inquirer");
 
 // const url = "https://www.codewars.com/collections/python-8kyu-1";
 // const folder_name = "8kyu";
-let url, folder_name;
 
 inquirer
   .prompt([
@@ -14,45 +13,73 @@ inquirer
       type: "input",
       name: "url",
       message: "Codewars collection url?",
-      validate: function(value) {
+      validate: function (value) {
         if (value !== "") {
           return true;
         }
         return "URL is required";
-      }
+      },
     },
     {
       type: "input",
       name: "folder_name",
       message: "Folder name for problems?",
-      validate: function(value) {
+      validate: function (value) {
         if (value !== "") {
           return true;
         }
         return "Folder name is required";
-      }
-    }
+      },
+    },
   ])
-  .then(answers => {
-    url = answers.url;
-    folder_name = answers.folder_name;
-
-    app(url, folder_name);
+  .then((answers) => {
+    app(answers.url, answers.folder_name);
   });
 
-function app(url, folder_name) {
-  puppeteer.launch().then(async browser => {
+function create_folfer(folder) {
+  let readme = "\n\n## " + folder + "\n";
+
+  fs.appendFileSync("readme.md", readme);
+
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
+}
+
+function write_file(folder, kata, index) {
+  let slug = slugify(kata.title, { remove: /[\/:*?"<>|]/g, lower: true });
+  let template = `\n${index + 1}. [${kata.title}](${
+    kata.url
+  }) - [Solution](${folder}/${index + 1}-${slug}.md)`;
+  fs.appendFileSync("readme.md", template);
+
+  let problemTemplate = "## " + kata.title + ":\n";
+  problemTemplate += "\n### Problem:\n";
+  problemTemplate += kata.description;
+  problemTemplate += "\n### Solution";
+
+  try {
+    fs.writeFileSync(`${folder}/${index + 1}-${slug}.md`, problemTemplate);
+  } catch (error) {
+    console.log(`ERROR --- ${folder}/${index + 1}-${slug}.md`);
+  }
+}
+
+function app(url, folder) {
+  puppeteer.launch().then(async (browser) => {
     const page = await browser.newPage();
     await page.goto(url);
-    let content = await page.content();
+    let list_page_content = await page.content();
     let katas = [];
 
-    $(".collection-items .item-title a", content).each(function() {
+    $(".collection-items .item-title a", list_page_content).each(function () {
       katas.push({
         url: "https://www.codewars.com" + $(this).attr("href"),
-        title: $(this).text()
+        title: $(this).text(),
       });
     });
+
+    create_folfer(folder);
 
     for (let i = 0; i < katas.length; i++) {
       const kata = katas[i];
@@ -67,38 +94,9 @@ function app(url, folder_name) {
       kata.description = description.html();
 
       console.log(i + 1, kata);
+
+      write_file(folder, kata, i);
     }
-
-    let readme = "\n\n## " + folder_name + "\n";
-
-    fs.appendFileSync("readme.md", readme);
-
-    if (!fs.existsSync(folder_name)) {
-      fs.mkdirSync(folder_name);
-    }
-
-    console.log("Writting data to files...");
-
-    katas.map((value, index) => {
-      let slug = slugify(value.title, { remove: /[\/:*?"<>|]/g, lower: true });
-      let template = `\n${index + 1}. [${value.title}](${
-        value.url
-      }) - [Solution](${folder_name}/${index + 1}-${slug}.md)`;
-      fs.appendFileSync("readme.md", template);
-
-      let problemTemplate = "### Problem:\n";
-      problemTemplate += value.description;
-      problemTemplate += "\n### Solution";
-
-      try {
-        fs.writeFileSync(
-          `${folder_name}/${index + 1}-${slug}.md`,
-          problemTemplate
-        );
-      } catch (error) {
-        console.log(`ERROR --- ${folder_name}/${index + 1}-${slug}.md`);
-      }
-    });
 
     await browser.close();
   });
